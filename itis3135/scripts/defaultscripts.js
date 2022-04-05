7f7610b4148de83b3f6bdabefb892258ffa81dc5
rachelwilliam <rwill239@uncc.edu>
2022-04-05 10:29:54 -0400

    js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -5,7 +5,7 @@ function scriptTest() {
 function welcome() {
     let name = document.getElementById("name").value;
     let feel = document.getElementById("feel").value;
-    document.getElementById("welcome").innerHTML = ("<p>The Ruby Wallaby welcomes you, " + name + "! It seems you are feeling " + feel + "!")
+    document.getElementById("welcome").innerHTML = ("<p>The Ruby Wallaby welcomes you, " + name + "! We're glad you are feeling " + feel + "!")
 }
 
 function getDate() {

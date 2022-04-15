11086afba11081c8ca088d3a6fd91770f655f993
rachelwilliam <rwill239@uncc.edu>
2022-04-15 14:09:44 -0400

    hobby

---
+++ itis3135/hobby/scripts/hobby.js
@@ -0,0 +1,12 @@
+function showhide() 
+{
+    var x = document.getElementById('SectionName');
+    if (x.style.display == "none") 
+    {
+        x.style.display = "block";
+    } 
+    else 
+    {
+        x.style.display = "none";
+    }
+}

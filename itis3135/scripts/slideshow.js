4721490929d5ada81fbc3158d31b3de9e29957f2
rachelwilliam <rwill239@uncc.edu>
2022-04-16 13:20:22 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,37 +1,28 @@
-let slideindex = 1;
-showslides(slideindex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusslides(n) 
-{
-    showslides(slideindex += n);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function currentslide(n) 
-{
-    showslides(slideindex = n);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showslides(n) 
-{
-    let i;
-    let slides = document.getElementsByClassName("myslides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) 
-    {
-        slideindex = 1
-    }
-    if (n < 1) 
-    {
-        slideindex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) 
-    {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) 
-    {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideindex-1].style.display = "block";
-    dots[slideindex-1].className += " active";
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }

20c955afd0848aebf526bd76a821d6e3c40efdab
rachelwilliam <rwill239@uncc.edu>
2022-04-18 17:08:54 -0400

    Update activity10.js

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,31 +1,30 @@
-$(document).ready(function() {
-    // get the image URL and caption for each image and animate the caption
+$(document).ready(function () {
+    (new Image()).src = this;
     $("#image_list a").each(function () {
-            
-            let caption = $(this).attr("title");
 
-            // preload the image for each link
-            let gallery = new Image();
-            gallery.src = $(this).attr("href");
+        let caption = $(this).attr("title");
+
+        // preload the image for each link
+        let gallery = new Image();
+        gallery.src = $(this).attr("href");
     });
-           
 
-            // set up the event handlers for each link
-            $("#image_list a").click(function(event){
-                    let imageURL = $(this).attr("href");
-                    $("#image").attr("src", imageURL);
+    // set up the event handlers for each link
+    $("#image_list a").click(function (event) {
+        let url = $(this).attr("href");
+        $("#image").attr("src", url);
 
-                    let caption = $(this).attr("title");
-                    $("#caption").text(caption);
+        let caption = $(this).attr("title");
+        $("#caption").text(caption);
 
-                    // cancel the default action of each link
-                    event.preventDefault();
+        // cancel the default action of each link
+        event.preventDefault();
 
-                     });
-                     
-                     
-              // move the focus to the first link
-    $("li:first-child a").focus();                                                                                                                                                                                 
+    });
 
-    
+
+    // move the focus to the first link
+    $("li:first-child a").focus();
+
+
 }); // end ready

0d0c5b1e5193684a3af10832e2376e5ce5e8cfe4
rachelwilliam <rwill239@uncc.edu>
2022-04-18 18:07:31 -0400

    Update activity10.js

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     (new Image()).src = this;
-    
-    $("#image_list a").each(function () {
+
+    $("a").each(function () {
         // preload the image for each link
         let gallery = new Image();
         gallery.src = $(this).attr("href");
     });
 
     // set up the event handlers for each link
-    $("#image_list a").click(function (evt) {
+    $("a").click(function(evt) {
         let url = $(this).attr("href");
         $("#image").attr("src", url);
 

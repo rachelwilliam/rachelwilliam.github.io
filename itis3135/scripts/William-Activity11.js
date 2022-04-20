90aa624bdcdafeb1bb3a567f66c381fd4bdba16d
rachelwilliam <rwill239@uncc.edu>
2022-04-20 11:17:44 -0400

    act11

---
+++ itis3135/scripts/William-Activity11.js
@@ -0,0 +1,7 @@
+//<!-- jQuery call to the accordion() method. -->
+$(document).ready(function() {
+    $("#tabs").accordian({
+        heightStyle: "content",
+        collapsible: true
+    });
+});

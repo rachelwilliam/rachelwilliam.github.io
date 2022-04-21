1b2327b3c3a584099a9c53ce3640ff4efd206712
rachelwilliam <rwill239@uncc.edu>
2022-04-21 17:28:18 -0400

    Update William-Activity12.js

--- itis3135/scripts/William-Activity12.js
+++ itis3135/scripts/William-Activity12.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
         randomStart: true,
         minSlides: 1,
         maxSlides: 1,
-        slideWidth: 300,
-        slideMargin: 20
+        slideWidth: 500,
+        slideMargin: 20,
+        moveSlides: 1,
+        captions: true,
+        speed: 3000,
+        pager: true,
+        pagerType: "short",
+        pager_selector: "#id_pager"
     });
 });

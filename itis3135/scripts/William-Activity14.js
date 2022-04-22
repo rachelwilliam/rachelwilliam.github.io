a2c8956bde02d913965c42a3158b85206fb41019
rachelwilliam <rwill239@uncc.edu>
2022-04-22 12:28:59 -0400

    Update William-Activity14.js

--- itis3135/scripts/William-Activity14.js
+++ itis3135/scripts/William-Activity14.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+    $("#nav_list li").click(function () {
+        $.ajax({
+            type: "get",
+            url: "json_files/" + $(this).attr("title") + ".json",
+            error: function (xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function (data) {
+                $.each(data, function () {
+                    $.each(this, function (key, value) {
+                        $("main").text("");
+                        $("main").append(
+                            "<h1>" + value.title + "</h1>" +
+                            "<img src=\"" + value.image + "\">" +
+                            "<h2>" + value.month + "</h2>" +
+                            "<h2>" + value.speaker + "</h2>" +
+                            "<p>" + value.text + "</p>"
+                        );
+                    });
+                });
+            }
+        });
+    });
 }); // end ready

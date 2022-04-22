8353114fdc585d18a6b9e587c26cb26ecda190b0
rachelwilliam <rwill239@uncc.edu>
2022-04-22 19:52:33 -0400

    Update William-Activity11.js

--- itis3135/scripts/William-Activity11.js
+++ itis3135/scripts/William-Activity11.js
@@ -1,7 +1,6 @@
 //<!-- jQuery call to the accordion() method. -->
 $(document).ready(function() {
     $("#accordian").accordian({
-        active: false,
         heightStyle: "content",
         collapsible: true
     });

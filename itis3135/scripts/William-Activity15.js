33dd9ebffae500e0c78402c27b658137ae9df842
rachelwilliam <rwill239@uncc.edu>
2022-04-24 18:20:44 -0400

    Update William-Activity15.js

--- itis3135/scripts/William-Activity15.js
+++ itis3135/scripts/William-Activity15.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 $.each(this, function (key, value) {
                     $("#faculty").append(
                         "<img src=" + value.image +"></img>" +
-                        "<h2>" + value.name + "</h2>" +
+                        "<h2>" + value.full_name + "</h2>" +
                         "<h3>" + value.department + "</h3>" +
                         "<p>" + value.bio + "</p>" +
                         "<br>"

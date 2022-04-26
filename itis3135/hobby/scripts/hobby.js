152e27905b850500e90e7b27833e30b847e91d14
rachelwilliam <rwill239@uncc.edu>
2022-04-25 21:56:56 -0400

    hobby

--- itis3135/hobby/scripts/hobby.js
+++ itis3135/hobby/scripts/hobby.js
@@ -1,4 +1,4 @@
-function showhide(value) 
+/*function showhide(value) 
 {
     var pages = document.getElementByClassName("pages");
     document.getElementById(value).style.display = 'block';
@@ -9,4 +9,14 @@ function showhide(value)
             pages[i].style.display = 'none';
         }
     }
-}
+}*/
+
+function show(shown, hidden1, hidden2, hidden3, hidden4, hidden5) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden1).style.display='none';
+    document.getElementById(hidden2).style.display='none';
+    document.getElementById(hidden3).style.display='none';
+    document.getElementById(hidden4).style.display='none';
+    document.getElementById(hidden5).style.display='none';
+    return false;
+  }

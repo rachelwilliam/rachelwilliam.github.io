18a14d58a28c4816e50c74ea893dd27f8a96e10f
rachelwilliam <rwill239@uncc.edu>
2022-05-08 21:47:34 -0400

    ajax

--- itis3135/project/scripts/time.js
+++ itis3135/project/scripts/time.js
@@ -1,10 +1,10 @@
-function loadDoc() {
+function loadsubmit() {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("submittext").innerHTML = this.responseText;
       }
     };
-    req.open("POST", "demo_post.txt", true);
+    req.open("POST", "demopost.txt", true);
     req.send();
 }
